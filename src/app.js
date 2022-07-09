73619482341c49e69cf10df93b99674d4a2ac467
KarolaBokeh <karolabokeh@protonmail.com>
2022-07-09 10:00:02 -0400

    Started working on JS

--- src/app.js
+++ src/app.js
@@ -1,2 +1,4 @@
 let apiKey = "e0d7a426efb522a611a5ee20482de773";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&apiid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q={NewYork}&appid=${apiKey}`;
+
+console.log(apiUrl);
